550133f583b38feb58a92909dd7c9f37f59f34d8
willl85 <ngowilfried@gmail.com>
2023-08-17 16:28:54 +0200

    Fecth data in form

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,20 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from '../UI/Card';
 import classes from './AddUser.module.css';
 import Button from '../UI/Button';
 
 const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState('');
+  const [enteredAge, setEnteredAge] = useState('');
+
   const addUserHandler = (e) => {
     e.preventDefault();
+    console.log(enteredUsername, enteredAge)
   };
+
+  const usernameChangeHandler = (e) => {
+    setEnteredUsername(e.target.value);
+  }
+
+  const ageChangeHandler = (e) => {
+    setEnteredAge(e.target.value);
+  }
 
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
+        <input id="username" type="text" onChange={usernameChangeHandler} />
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" />
+        <input id="age" type="number" onChange={ageChangeHandler} />
         <Button type="submit"> Add user</Button>
       </form>
     </Card>
